302d3d511b5b6259704d317d780a3e9954cd3d56
skleee <39164907+skleee@users.noreply.github.com>
2018-11-07 03:47:13 +0900

    week4

---
+++ week4/jqeryExercise.js
@@ -0,0 +1,8 @@
+$("div").css("background", "purple");
+
+$("div.highlight").css("width", "200px");
+
+$("#third").css("border", "4px solid orange");
+
+// first div only and change its font color to pin
+$("div:first-of-type").css("color", "pink");
